a99a900b14486550c5af778faefac765de700652
chankaran2996 <chandrasekaran2996@gmail.com>
2025-04-03 12:26:42 +0530

    1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import PrivateRoute from './routes/PrivateRoute'
+import AdminDashboard from './pages/AdminDashboard'
+import ManageTask from './pages/ManageTask'
+import CreateTask from './pages/CreateTask'
+import ManageUser from './pages/ManageUser'
+import UserDashboard from './pages/UserDashboard'
+import MyTask from './pages/MyTask'
+import ViewTaskDetalis from './pages/ViewTaskDetalis'
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login/>} />
+          <Route path='/register' element={<Register/>} />
+          {/* Admin route */}
+          <Route element={<PrivateRoute allowRoles={["admin"]} />}>
+            <Route path='/admin/dashboard' element={<AdminDashboard />} />
+            <Route path='/admin/task' element={<ManageTask />} />
+            <Route path='/admin/create-task' element={<CreateTask />} />
+            <Route path='/admin/user' element={<ManageUser />} />
+          </Route>
+          {/* user route */}
+          <Route element={<PrivateRoute allowRoles={["admin"]} />}>
+            <Route path='/user/dashboard' element={<UserDashboard />} />
+            <Route path='/user/tasks' element={<MyTask />} />
+            <Route path='/user/task-detials/:id' element={<ViewTaskDetalis />} />
+            {/* <Route path='/admin/user' element={<ManageUser />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/pages/ViewTaskDetalis.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewTaskDetalis = () => {
+  return (
+    <div>
+      ViewTaskDetalis
+    </div>
+  )
+}
+
+export default ViewTaskDetalis
